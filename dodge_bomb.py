42d6bac8c9b7ac665814d2b685d295888151fff6
gentletan <c0b220981a@edu.teu.ac.jp>
2023-06-20 14:12:45 +0900

    練習1終了

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,8 +1,9 @@
+import random
 import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 1200, 700
 
 
 def main():
@@ -11,15 +12,22 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bomb_surface = pg.Surface((20, 20))
+    pg.draw.circle(bomb_surface, (255, 0, 0), (10, 10), 10)
+    bomb_surface.set_colorkey((0, 0, 0))
+    bomb_rect = bomb_surface.get_rect()
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+        
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bomb_rect.center = random.randint(0,WIDTH), random.randint(0, HEIGHT)
+        screen.blit(bomb_surface, bomb_rect)
         pg.display.update()
         tmr += 1
         clock.tick(10)
